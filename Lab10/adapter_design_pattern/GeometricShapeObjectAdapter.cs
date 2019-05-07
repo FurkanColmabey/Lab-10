99f01a7a3a63f58492b13b459c87b239209c4d1d
FurkanColmabey <33626342+FurkanColmabey@users.noreply.github.com>
2019-05-07 22:05:14 +0300

    Adapter

---
+++ Lab10/adapter_design_pattern/GeometricShapeObjectAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adapter_design_pattern
+{
+    class GeometricShapeObjectAdapter : Shape
+    {
+        public GeometricShape sekiller;
+
+        public GeometricShapeObjectAdapter(GeometricShape shape)
+        {
+            sekiller = shape;
+        }
+        public void draw( )
+        {
+            sekiller.drawShape();
+        }        
+            
+    }
+}
---
+++ Lab10/adapter_design_pattern/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adapter_design_pattern
+{
+    class Rectangle :Shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("Drawign Rectangle");
+        }
+    }
+}
